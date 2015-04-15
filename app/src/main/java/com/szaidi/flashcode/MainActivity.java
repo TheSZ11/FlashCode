abac42cc8698305990fd8877ba2df2c4f095ed34
TheSZ11 <sajjadzaidi11@gmail.com>
2015-04-15 19:52:00 -0400

    implemented flashlight turning on based on morse code '-' or '.'

--- app/src/main/java/com/szaidi/flashcode/MainActivity.java
+++ app/src/main/java/com/szaidi/flashcode/MainActivity.java
@@ -1,23 +1,37 @@
 package com.szaidi.flashcode;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Camera;
-import android.support.v7.app.ActionBarActivity;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.lang.Thread;
+import java.util.jar.JarEntry;
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
     EditText editText;
+    private Camera camera;
+    private boolean isFlashOn = false;
+
+    protected void onStop() {
+        super.onStop();
+        if (camera != null) {
+            camera.release();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +41,18 @@ public class MainActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         editText = (EditText) findViewById(R.id.editText);
-
         Button mButton = (Button) findViewById(R.id.button);
+        Context context = this;
+        PackageManager pm = context.getPackageManager();
+
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            Log.e("err", "Device has no camera!");
+            return;
+        }
+
+        camera = Camera.open();
+        final Parameters p = camera.getParameters();
+
         mButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -38,14 +62,73 @@ public class MainActivity extends ActionBarActivity {
                 String morseLetter;
 
                 for (int i = 0; i < text.length(); i++) {
-                    letter = text.charAt(i)+ "";
+                    letter = text.charAt(i) + "";
                     morseLetter = convert(letter);
+
+                    Toast.makeText(getApplicationContext(), morseLetter, Toast.LENGTH_SHORT).show();
+
+                    for (int j = 0; j < morseLetter.length(); j++) {
+                        if (morseLetter.charAt(j) == '-') {
+                            if (!isFlashOn) {
+                                p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                                camera.setParameters(p);
+                                camera.startPreview();
+                                isFlashOn = true;
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                    // handle the exception...
+                                    // For example consider calling Thread.currentThread().interrupt(); here.
+                                    Thread.currentThread().interrupt();
+                                }
+                                p.setFlashMode(Parameters.FLASH_MODE_OFF);
+                                camera.setParameters(p);
+                                camera.stopPreview();
+                                isFlashOn = false;
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                    // handle the exception...
+                                    // For example consider calling Thread.currentThread().interrupt(); here.
+                                    Thread.currentThread().interrupt();
+                                }
+                            }
+                        } else if (morseLetter.charAt(j) == '.') {
+                            if (!isFlashOn) {
+                                p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                                camera.setParameters(p);
+                                camera.startPreview();
+                                isFlashOn = true;
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                    // handle the exception...
+                                    // For example consider calling Thread.currentThread().interrupt(); here.
+                                    Thread.currentThread().interrupt();
+                                }
+                                p.setFlashMode(Parameters.FLASH_MODE_OFF);
+                                camera.setParameters(p);
+                                camera.stopPreview();
+                                isFlashOn = false;
+                                try {
+                                    Thread.sleep(500);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                    // handle the exception...
+                                    // For example consider calling Thread.currentThread().interrupt(); here.
+                                    Thread.currentThread().interrupt();
+                                }
+                            }
+                        }
+
+                    }
 
                 }
             }
         });
-
-
 
 
     }
@@ -53,77 +136,77 @@ public class MainActivity extends ActionBarActivity {
     private String convert(String letter) {
         String convertedMorse = null;
 
-        if(letter.equalsIgnoreCase("a"))
+        if (letter.equalsIgnoreCase("a"))
             convertedMorse = ".-";
-        if(letter.equalsIgnoreCase("b"))
+        if (letter.equalsIgnoreCase("b"))
             convertedMorse = "-...";
-        if(letter.equalsIgnoreCase("c"))
+        if (letter.equalsIgnoreCase("c"))
             convertedMorse = "-.-.";
-        if(letter.equalsIgnoreCase("d"))
+        if (letter.equalsIgnoreCase("d"))
             convertedMorse = "-..";
-        if(letter.equalsIgnoreCase("e"))
+        if (letter.equalsIgnoreCase("e"))
             convertedMorse = ".";
-        if(letter.equalsIgnoreCase("f"))
+        if (letter.equalsIgnoreCase("f"))
             convertedMorse = "..-.";
-        if(letter.equalsIgnoreCase("g"))
+        if (letter.equalsIgnoreCase("g"))
             convertedMorse = "--.";
-        if(letter.equalsIgnoreCase("h"))
+        if (letter.equalsIgnoreCase("h"))
             convertedMorse = "....";
-        if(letter.equalsIgnoreCase("i"))
+        if (letter.equalsIgnoreCase("i"))
             convertedMorse = "..";
-        if(letter.equalsIgnoreCase("j"))
+        if (letter.equalsIgnoreCase("j"))
             convertedMorse = ".---";
-        if(letter.equalsIgnoreCase("k"))
+        if (letter.equalsIgnoreCase("k"))
             convertedMorse = "-.-";
-        if(letter.equalsIgnoreCase("l"))
+        if (letter.equalsIgnoreCase("l"))
             convertedMorse = ".-..";
-        if(letter.equalsIgnoreCase("m"))
+        if (letter.equalsIgnoreCase("m"))
             convertedMorse = "--";
-        if(letter.equalsIgnoreCase("n"))
+        if (letter.equalsIgnoreCase("n"))
             convertedMorse = "-.";
-        if(letter.equalsIgnoreCase("o"))
+        if (letter.equalsIgnoreCase("o"))
             convertedMorse = "---";
-        if(letter.equalsIgnoreCase("p"))
+        if (letter.equalsIgnoreCase("p"))
             convertedMorse = ".--.";
-        if(letter.equalsIgnoreCase("q"))
+        if (letter.equalsIgnoreCase("q"))
             convertedMorse = "--.-";
-        if(letter.equalsIgnoreCase("r"))
+        if (letter.equalsIgnoreCase("r"))
             convertedMorse = ".-.";
-        if(letter.equalsIgnoreCase("s"))
+        if (letter.equalsIgnoreCase("s"))
             convertedMorse = "...";
-        if(letter.equalsIgnoreCase("t"))
+        if (letter.equalsIgnoreCase("t"))
             convertedMorse = "-";
-        if(letter.equalsIgnoreCase("u"))
+        if (letter.equalsIgnoreCase("u"))
             convertedMorse = "..-";
-        if(letter.equalsIgnoreCase("v"))
+        if (letter.equalsIgnoreCase("v"))
             convertedMorse = "...-";
-        if(letter.equalsIgnoreCase("w"))
+        if (letter.equalsIgnoreCase("w"))
             convertedMorse = ".--";
-        if(letter.equalsIgnoreCase("x"))
+        if (letter.equalsIgnoreCase("x"))
             convertedMorse = "-..-";
-        if(letter.equalsIgnoreCase("y"))
+        if (letter.equalsIgnoreCase("y"))
             convertedMorse = "-.--";
-        if(letter.equalsIgnoreCase("z"))
+        if (letter.equalsIgnoreCase("z"))
             convertedMorse = "--..";
-        if(letter.equals("1"))
+        if (letter.equals("1"))
             convertedMorse = ".----";
-        if(letter.equals("2"))
+        if (letter.equals("2"))
             convertedMorse = "..---";
-        if(letter.equals("3"))
+        if (letter.equals("3"))
             convertedMorse = "...--";
-        if(letter.equals("4"))
+        if (letter.equals("4"))
             convertedMorse = "....-";
-        if(letter.equals("5"))
+        if (letter.equals("5"))
             convertedMorse = ".....";
-        if(letter.equals("6"))
+        if (letter.equals("6"))
             convertedMorse = "-....";
-        if(letter.equals("7"))
+        if (letter.equals("7"))
             convertedMorse = "--...";
-        if(letter.equals("8"))
+        if (letter.equals("8"))
             convertedMorse = "---..";
-        if(letter.equals("9"))
+        if (letter.equals("9"))
             convertedMorse = "----.";
-        if(letter.equals("0"))
+        if (letter.equals("0"))
             convertedMorse = "-----";
 
         return convertedMorse;
